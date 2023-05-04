1cfe0a634625da960f5adfd6939fe854593f91fb
Vaughan Kitchen <vaughan.kitchen@streem.com.au>
2023-05-04 14:47:01 +1200

    New build config format. Linux and OSX binaries

--- build.zig
+++ build.zig
@@ -3,8 +3,18 @@ const Mode = @import("std").builtin.Mode;
 const CrossTarget = @import("std").zig.CrossTarget;
 
 pub fn build(b: *Builder) !void {
-    const search = b.addExecutable("search-recipes", "src/search.zig");
-    search.setTarget(try CrossTarget.parse(.{ .arch_os_abi = "x86_64-linux-musl" }));
-    search.setBuildMode(Mode.ReleaseFast);
-    search.install();
+    const search = b.addExecutable(.{
+        .name = "potatoes",
+        .root_source_file = .{ .path = "src/server.zig" },
+        .optimize = Mode.ReleaseSafe,
+    });
+    b.installArtifact(search);
+
+    const search_linux = b.addExecutable(.{
+        .name = "potatoes-linux",
+        .root_source_file = .{ .path = "src/server.zig" },
+        .target = try CrossTarget.parse(.{ .arch_os_abi = "x86_64-linux-musl" }),
+        .optimize = Mode.ReleaseFast,
+    });
+    b.installArtifact(search_linux);
 }
