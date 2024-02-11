74cd416cf01ec37058231005ace6d16a02643890
Vaughan Kitchen <contact@tophat.nz>
2024-02-11 15:20:33 +1300

    Decode some values

--- search.exs
+++ search.exs
@@ -5,4 +5,9 @@ opts = [:binary, active: false, reuseaddr: true]
 :ok = :gen_tcp.send(sock, <<0, 1, 5, 0, 0, 0, 3, 0, 99, 97, 116>>)
 {:ok, resp} = :gen_tcp.recv(sock, 0)
 
-IO.puts(resp)
+<<_version::size(16), total_results::native-integer-size(16), payload_results::native-integer-size(16), rest::binary>> = resp
+
+IO.puts("Total number of results: #{total_results}")
+IO.puts("Results received: #{payload_results}")
+
+IO.puts(rest)
