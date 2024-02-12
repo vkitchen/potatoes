0f2ffe7b4931f60e55da4f79e89a50044c00cae0
Vaughan Kitchen <contact@tophat.nz>
2024-02-13 11:00:02 +1300

    Destructure one result

--- search.exs
+++ search.exs
@@ -7,7 +7,11 @@ opts = [:binary, active: false, reuseaddr: true]
 
 <<_version::size(16), total_results::native-integer-size(16), payload_results::native-integer-size(16), rest::binary>> = resp
 
+<<url_len::native-integer-size(16), url::binary-size(url_len), title_len::native-integer-size(16), title::binary-size(title_len), snippet_len::native-integer-size(16), snippet::binary-size(snippet_len), _rest::binary>> = rest
+
 IO.puts("Total number of results: #{total_results}")
 IO.puts("Results received: #{payload_results}")
 
-IO.puts(rest)
+IO.puts(url)
+IO.puts(title)
+IO.puts(snippet)
