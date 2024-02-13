c78ace9fdbc22ce31fb89bfa8ac8ceec91bb81aa
Vaughan Kitchen <contact@tophat.nz>
2024-02-13 14:48:57 +1300

    Accept user input

--- search.exs
+++ search.exs
@@ -1,39 +1,43 @@
 #!/usr/bin/env elixir
 
-opts = [:binary, active: false, reuseaddr: true]
-{:ok, sock} = :gen_tcp.connect({:local, "/tmp/cocomel.sock"}, 0, opts)
-:ok = :gen_tcp.send(sock, <<0, 1, 5, 0, 0, 0, 3, 0, 99, 97, 116>>)
-{:ok, resp1} = :gen_tcp.recv(sock, 0)
-{:ok, resp2} = :gen_tcp.recv(sock, 0)
-{:ok, resp3} = :gen_tcp.recv(sock, 0)
-{:error, :closed} = :gen_tcp.recv(sock, 0)
+defmodule Search do
+  defp recv(sock) do
+    case :gen_tcp.recv(sock, 0) do
+      {:ok, resp} ->
+        resp <> recv(sock)
+      {:error, :closed} ->
+        <<>>
+    end
+  end
 
-resp = resp1 <> resp2 <> resp3
+  def search(query) do
+    opts = [:binary, active: false, reuseaddr: true]
+    {:ok, sock} = :gen_tcp.connect({:local, "/tmp/cocomel.sock"}, 0, opts)
+    :ok = :gen_tcp.send(sock, <<0, 1, 5::native-16, 0::native-16, String.length(query)::native-16, query::binary>>)
 
-<<_version::size(16), total_results::native-integer-size(16), payload_results::native-integer-size(16), payload::binary>> = resp
+    recv(sock)
+  end
 
-defmodule Foo do
   def parse(<<>>), do: []
 
-  def parse(<<url_len::native-integer-size(16), url::binary-size(url_len), title_len::native-integer-size(16), title::binary-size(title_len), snippet_len::native-integer-size(16), snippet::binary-size(snippet_len), tail::binary>>) do
+  def parse(<<url_len::native-16, url::binary-size(url_len), title_len::native-16, title::binary-size(title_len), snippet_len::native-16, snippet::binary-size(snippet_len), tail::binary>>) do
     [[url, title, snippet] | parse(tail)]
   end
 
   def print([]), do: nil
 
   def print([[url, title, snippet] | tail]) do
-    IO.puts(url)
-    IO.puts(title)
-    IO.puts(snippet)
+    IO.puts("#{title}\n#{url}\n#{snippet}\n")
 
     print(tail)
   end
-
-
 end
 
-IO.puts("Total number of results: #{total_results}")
-IO.puts("Results received: #{payload_results}")
+query = IO.gets("Query> ")
 
-results = Foo.parse(payload)
-Foo.print(results)
+<<_version::16, total_results::native-16, payload_results::native-16, payload::binary>> = Search.search(query)
+
+IO.puts("Showing #{payload_results}/#{total_results}")
+
+results = Search.parse(payload)
+Search.print(results)
