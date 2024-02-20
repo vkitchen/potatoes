676022c3ba1e08fa0da3e04f2eeab1fd4c87d543
Vaughan Kitchen <contact@tophat.nz>
2024-02-21 11:56:58 +1300

    Connect to Cocomel from GenServer

--- lib/cocomel.ex
+++ lib/cocomel.ex
@@ -9,13 +9,33 @@ defmodule Cocomel do
     {:ok, state}
   end
 
+  defp recv(sock) do
+    case :gen_tcp.recv(sock, 0) do
+      {:ok, resp} ->
+        resp <> recv(sock)
+      {:error, :closed} ->
+        <<>>
+    end
+  end
+
+  defp parse(<<>>), do: []
+
+  defp parse(<<url_len::native-16, url::binary-size(url_len), title_len::native-16, title::binary-size(title_len), snippet_len::native-16, snippet::binary-size(snippet_len), tail::binary>>) do
+    [[url, title, snippet] | parse(tail)]
+  end
+
   def handle_call({:search, query}, _from, state) do
-    {:reply, query, state}
+    opts = [:binary, active: false, reuseaddr: true]
+    {:ok, sock} = :gen_tcp.connect({:local, "/tmp/cocomel.sock"}, 0, opts)
+    :ok = :gen_tcp.send(sock, <<0, 1, 5::native-16, 0::native-16, String.length(query)::native-16, query::binary>>)
+
+    <<_version::16, total_results::native-16, payload_results::native-16, payload::binary>> = recv(sock)
+    results = parse(payload)
+
+    {:reply, results, state}
   end
 
   def search(query) do
     GenServer.call(__MODULE__, {:search, query})
   end
 end
-
-
